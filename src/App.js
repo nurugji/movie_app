e7d619f39b3660f8aca83528a7794df64f6c6b38
pingu <nurugji@gmail.com>
2021-10-14 21:50:11 +0900

    Nomad React 4.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
   getMovies = async () => {
-    const moives = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({
+      movies,
+      isLoading: false,
+    });
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Lodaing" : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Lodaing"
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              ></Movie>
+            ))}
+      </div>
+    );
   }
 }
 
