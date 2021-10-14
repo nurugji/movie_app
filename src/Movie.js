f9282e50d8d322a37d5691bad480af8aeddc89ed
pingu <nurugji@gmail.com>
2021-10-14 22:54:28 +0900

    Nomad React 4.3

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie_data">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie_year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="genres">
+          {genres.map((item, index) => (
+            <li key={index} className="genres_genres">
+              {item}
+            </li>
+          ))}
+        </ul>
+        <p className="movie_summary">{summary}</p>
       </div>
     </div>
   );
@@ -21,6 +28,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
