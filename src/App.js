480d1981056c25a2e92aeb28c9252767bc9eddf2
pingu <nurugji@gmail.com>
2021-10-14 19:05:22 +0900

    Nomad React 4.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
   };
+  getMovies = async () => {
+    const moives = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        isLoading: false,
-      });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
