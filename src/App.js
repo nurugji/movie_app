5d2ae6e2d418f41f13e5244c2b9db891368e03dc
pingu <nurugji@gmail.com>
2021-10-16 14:50:37 +0900

    Nomad React 6.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routs/Home";
+import About from "./routs/About";
 
 function App() {
-  return <div>HELLO</div>;
+  return (
+    <HashRouter>
+      <Route path="/a" component={Home}></Route>
+      <Route path="/About" component={About}></Route>
+    </HashRouter>
+  );
 }
 export default App;
